1abdff054f1e4ea63884fc3f71684756b31d0456
Leevi Uotinen <uotinen.leevi@hotmail.com>
2019-10-06 10:01:25 +0300

    Add agency Query.

--- duely-graphql/src/graphql/schema/Query.js
+++ duely-graphql/src/graphql/schema/Query.js
@@ -5,20 +5,44 @@ export default {
   typeDef: `
     type Query {
       me: Subject
+      agency(uuid: ID, subdomainName: String): [Agency!]
     }
   `,
   resolvers: {
     Query: {
       async me(obj, args, context, info) {
         if (!context.jwt)
-          return null;
-  
+          throw new AuthenticationError('Unauthorized');
+
         const client = await pool.connect();
         try {
           await client.query('SELECT operation_.begin_session_($1::text, $2::text)', [context.jwt, context.ip]);
           const res = await client.query('SELECT * FROM operation_.query_active_subject_()');
           await client.query('SELECT operation_.end_session_()');
           return res.rows ? res.rows[0] : null;
+        } catch (error) {
+          throw new AuthenticationError(error.message);
+        }
+        finally {
+          client.release();
+        }
+      },
+      async agency(obj, { uuid, subdomainName }, context, info) {
+        if (!context.jwt)
+          throw new AuthenticationError('Unauthorized');
+
+        const client = await pool.connect();
+        try {
+          await client.query('SELECT operation_.begin_session_($1::text, $2::text)', [context.jwt, context.ip]);
+
+          const res = !uuid && !subdomainName
+            ? await client.query('SELECT * FROM operation_.query_agency_()')
+            : uuid
+              ? await client.query('SELECT * FROM operation_.query_agency_($1::uuid)', [uuid])
+              : await client.query('SELECT * FROM operation_.query_agency_by_subdomain_name_($1::text)', [subdomainName])
+
+          await client.query('SELECT operation_.end_session_()');
+          return res.rows;
         } catch (error) {
           throw new AuthenticationError(error.message);
         }
