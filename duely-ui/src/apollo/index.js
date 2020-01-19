2c3e8fa4b097bc8b54cff728a26fa71c5b40c402
Leevi Uotinen <uotinen.leevi@hotmail.com>
2020-01-19 17:44:02 +0200

    Add WebSocket support on client side.

--- duely-ui/src/apollo/index.js
+++ duely-ui/src/apollo/index.js
@@ -2,10 +2,11 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
 import { onError } from 'apollo-link-error';
-import { ApolloLink, execute, toPromise } from 'apollo-link';
-import { createHttpLink } from 'apollo-link-http';
-//import { WebSocketLink } from 'apollo-link-ws';
-//import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { ApolloLink, split, execute, toPromise } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 import gql from 'graphql-tag';
 import defaults from './defaults';
 import resolvers from './resolvers';
@@ -27,40 +28,71 @@ const cache = new InMemoryCache({
 
 cache.writeData({ data: defaults });
 
-const httpLink = createHttpLink({ uri: 'https://api.duely.app/graphql' });
+const httpLink = new HttpLink({ uri: 'https://api.duely.app/graphql' });
 
 let beginVisitPromise = null;
 
-const authLink = setContext(async (req, { headers }) => {
+async function getToken() {
   let token = localStorage.getItem('user-jwt') || localStorage.getItem('visitor-jwt');
 
-  if (!token) {
-    if (!beginVisitPromise) {
-      beginVisitPromise = toPromise(
-        execute(httpLink, {
-          query: gql`
-            mutation {
-              beginVisit {
-                success
-                message
-                jwt
-              }
-            }
-          `
-        }))
-        .then(({ data }) => {
-          if (data.beginVisit.success)
-            localStorage.setItem('visitor-jwt', data.beginVisit.jwt);
-          else {
-            // eslint-disable-next-line
-            console.log(data.beginVisit.message);
-          }
-        });
-    }
+  if (token)
+    return token;
 
-    await beginVisitPromise;
-    token = localStorage.getItem('visitor-jwt');
+  if (!beginVisitPromise) {
+    beginVisitPromise = toPromise(
+      execute(httpLink, {
+        query: gql`
+          mutation {
+            beginVisit {
+              success
+              message
+              jwt
+            }
+          }
+        `
+      }))
+      .then(({ data }) => {
+        if (data.beginVisit.success)
+          localStorage.setItem('visitor-jwt', data.beginVisit.jwt);
+        else {
+          // eslint-disable-next-line
+          console.log(data.beginVisit.message);
+        }
+      });
   }
+
+  await beginVisitPromise;
+  return localStorage.getItem('visitor-jwt');
+}
+
+const wsClient = new SubscriptionClient('wss://api.duely.app/graphql', {
+  reconnect: true,
+  lazy: true,
+  connectionParams: async () => {
+    const token = await getToken();
+    return (token
+      ? {
+            authorization: `Bearer ${token}`
+      }
+      : {});
+  }
+});
+const wsLink = new WebSocketLink(wsClient);
+const transportLink = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const authLink = setContext(async (req, { headers }) => {
+  let token = await getToken();
 
   // return the headers to the context so httpLink can read them
   return (token
@@ -91,7 +123,7 @@ const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) 
     }
   }
 
-  if (networkError) 
+  if (networkError)
     // eslint-disable-next-line
     console.log(`[Network error]: ${networkError}`);
 });
@@ -100,13 +132,22 @@ const client = new ApolloClient({
   link: ApolloLink.from([
     errorLink,
     authLink,
-    httpLink
+    transportLink
   ]),
   cache,
   resolvers
 });
 
-client.onResetStore(() => cache.writeData(defaults));
+client.onResetStore(() => {
+  cache.writeData(defaults);
+});
+
+client.onClearStore(() => {
+  // Close socket connection which will also unregister subscriptions on the server-side.
+  wsClient.close();
+  // Reconnect to the server.
+  wsClient.connect();
+});
 
 export {
   client,
