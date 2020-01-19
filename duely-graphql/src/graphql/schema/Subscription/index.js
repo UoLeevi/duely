6113acb607390a39950558a44156d6d805125ebf
Leevi Uotinen <uotinen.leevi@hotmail.com>
2020-01-19 15:02:30 +0200

    Setup agencyCreated subscription filtering.

--- duely-graphql/src/graphql/schema/Subscription/index.js
+++ duely-graphql/src/graphql/schema/Subscription/index.js
@@ -1,24 +1,4 @@
-//import { withFilter } from 'apollo-server';
-import { getSharedClient } from '../../../db';
-import pubsub from '../../pubsub';
-
-const AGENCY_CREATED = 'AGENCY_CREATED';
-
-getSharedClient().then(client => {
-  client.query('LISTEN "application_.agency_"');
-  client.on('notification', ({ channel, payload }) => {
-    if (channel !== 'application_.agency_')
-      return;
-
-    const { op, row } = JSON.parse(payload);
-
-    if (op !== 'I')
-      return;
-
-    pubsub.publish(AGENCY_CREATED, { agencyCreated: row });
-    console.log(row);
-  });
-});
+import agencyCreated from './agencyCreated';
 
 export default {
   typeDef: `
@@ -28,11 +8,7 @@ export default {
   `,
   resolvers: {
     Subscription: {
-      agencyCreated: {
-        subscribe() {
-          return pubsub.asyncIterator([AGENCY_CREATED]);
-        }
-      }
+      agencyCreated
     }
   }
 };
--- duely-graphql/src/graphql/subscriptions.js
+++ duely-graphql/src/graphql/subscriptions.js
@@ -1,14 +1,18 @@
-import { pool } from '../db';
+import { AuthenticationError } from 'apollo-server-core';
 
 export default {
   onConnect: (connectionParams, webSocket) => {
-    if (!connectionParams.jwt)
+    const authorization = connectionParams.authorization || null;
+    const jwt = (authorization && authorization.startsWith('Bearer ')
+      ? authorization.substring(7)
+      : null);
+
+    if (!jwt)
       throw new AuthenticationError('Unauthorized');
 
-    console.log(webSocket);
-
     return {
-      jwt
+      jwt,
+      ip: webSocket.upgradeReq.client.remoteAddress
     };
   }
 };
