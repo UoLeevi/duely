551a6a7df388f7519158d7c45892e89b76f02d07
Leevi Uotinen <uotinen.leevi@hotmail.com>
2020-02-02 13:54:10 +0200

    Adapt to changed query_service_ API.

--- duely-graphql/src/graphql/schema/AgencyServicesConnection.js
+++ duely-graphql/src/graphql/schema/AgencyServicesConnection.js
@@ -16,7 +16,7 @@ export default {
         const client = await pool.connect();
         try {
           await client.query('SELECT operation_.begin_session_($1::text, $2::text)', [context.jwt, context.ip]);
-          const res = await client.query('SELECT * FROM operation_.query_service_($1::uuid, $2::text[])', [connection.agencyUuid, status ? [status] : null]);
+          const res = await client.query('SELECT * FROM operation_.query_service_by_agency_($1::uuid, $2::text[])', [connection.agencyUuid, status ? [status] : null]);
           await client.query('SELECT operation_.end_session_()');
           return res.rows.map(row => ({
             agencyUuid: connection.agencyUuid,
