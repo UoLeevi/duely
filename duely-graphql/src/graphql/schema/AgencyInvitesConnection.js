616ac2b7acbd09f2d10186223643914e07c07757
Leevi Uotinen <uotinen.leevi@hotmail.com>
2020-03-08 18:28:26 +0200

    Fix spelling error while using query_user_invite_by_agency_.

--- duely-graphql/src/graphql/schema/AgencyInvitesConnection.js
+++ duely-graphql/src/graphql/schema/AgencyInvitesConnection.js
@@ -16,7 +16,7 @@ export default {
         const client = await pool.connect();
         try {
           await client.query('SELECT operation_.begin_session_($1::text, $2::text)', [context.jwt, context.ip]);
-          const res = await client.query('SELECT * FROM operation_.query_user_invites_by_agency_($1::uuid)', [connection.agencyUuid]);
+          const res = await client.query('SELECT * FROM operation_.query_user_invite_by_agency_($1::uuid)', [connection.agencyUuid]);
           await client.query('SELECT operation_.end_session_()');
           return res.rows.map(row => ({
             agencyUuid: connection.agencyUuid,
