8982372d3c0b3c08d0400277cbc77f9b03d06855
Leevi Uotinen <uotinen.leevi@hotmail.com>
2020-03-14 09:55:44 +0200

    Allow log in and sign up even when user is already logged in.

--- duely-postgres/duely-migrate.sh
+++ duely-postgres/duely-migrate.sh
@@ -1,4 +1,4 @@
-kubectl cp C:\Users\uotin\duely\duely\duely-postgres\duely-migrate.sql  duely-postgres-master-statefulset-0:duely-migrate.sql
+kubectl cp C:\Users\uotin\duely\duely\duely-postgres\duely-migrate.sql duely-postgres-master-statefulset-0:duely-migrate.sql
 kubectl exec -it duely-postgres-master-statefulset-0 bash
 psql -U postgres -d duely -f duely-migrate.sql
 rm duely-migrate.sql
--- duely-postgres/duely-migrate.sql
+++ duely-postgres/duely-migrate.sql
@@ -14,25 +14,8 @@ DECLARE
 BEGIN
 -- MIGRATION CODE START
 
-CREATE OR REPLACE FUNCTION operation_.query_user_invite_by_subject_(_subject_uuid uuid, _status text[] DEFAULT NULL::text[]) RETURNS TABLE(uuid_ uuid, agency_uuid_ uuid, inviter_uuid_ uuid, invitee_email_address_ text, role_uuid_ uuid, status_ text, status_at_ timestamp with time zone)
-    LANGUAGE plpgsql SECURITY DEFINER
-    AS $$
-DECLARE
-  _arg RECORD;
-BEGIN
-  SELECT _subject_uuid subject_uuid_, _status invite_status_ INTO _arg;
-  PERFORM security_.control_operation_('query_user_invite_by_subject_', _arg);
-
-  RETURN QUERY
-  SELECT ui.uuid_, ui.agency_uuid_, ui.inviter_uuid_, ui.invitee_email_address_, ui.role_uuid_, ui.status_, ui.status_at_
-  FROM application_.user_invite_ ui
-  JOIN security_.user_ u ON ui.invitee_email_address_ = u.email_address_
-  WHERE u.uuid_ = _subject_uuid
-    AND (_status IS NULL 
-     OR ui.status_ = ANY (COALESCE(_status, '{}'::text[])));
-
-END
-$$;
+PERFORM security_.implement_policy_allow_('sign_up_user_', 'logged_in_');
+PERFORM security_.implement_policy_allow_('log_in_user_', 'logged_in_');
 
 -- MIGRATION CODE END
 EXCEPTION WHEN OTHERS THEN
