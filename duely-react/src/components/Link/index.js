64b20a5675e10e3dc4bc670ae711b0e196ea13d1
Leevi Uotinen <uotinen.leevi@hotmail.com>
2020-07-31 21:17:11 +0300

    Make link component handle external domains and duely subdomains.

--- duely-react/src/components/Link/index.js
+++ duely-react/src/components/Link/index.js
@@ -2,19 +2,54 @@ import React from 'react';
 import { createPath, parsePath } from 'history';
 import useAppState from 'hooks/useAppState';
 
-const Link = ({ to, end, children, ...props }) => {
+function useLinkProps(to, props) {
   const [, send] = useAppState();
 
-  const path = typeof to === 'string' ? to : createPath(to);
-  const location = parsePath(path);
+  if (typeof to === 'object') {
+    to = createPath(to);
+  }
 
-  const handleClick = e => {
+  const url = new URL(to, window.location.origin);
+
+  if (url.origin !== window.location.origin) {
+    return { ...props, href: url.href };
+  }
+
+  // subdomain search param is used to indicate a redirect to another duely domain.
+  const subdomain = url.searchParams.get('subdomain');
+
+  if (subdomain) {
+    if (process.env.NODE_ENV === 'production') {
+      url.searchParams.delete('subdomain');
+
+      if (!url.searchParams.has('access_token')) {
+        const access_token = localStorage.getItem('user-jwt');
+
+        if (access_token) {
+          url.searchParams.set('access_token', encodeURIComponent(access_token));
+        }
+      }
+
+      url.hostname = `${subdomain}.duely.app`;
+    }
+
+    return { ...props, href: url.href };
+  }
+
+  const onClick = e => {
     e.preventDefault();
+    const location = parsePath(to);
     send({ type: 'NAVIGATION_REQUESTED', location });
   };
 
+  return { ...props, href: to, onClick };
+}
+
+const Link = ({ to, end, children, ...props }) => {
+  props = useLinkProps(to, props);
+
   return (
-    <a href={ path } { ...props } onClick={ handleClick }>
+    <a { ...props }>
       { children }
     </a>
   );
