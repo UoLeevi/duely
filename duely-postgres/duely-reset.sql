e2eba6a20f658ec7652090c8bd23f526638dd3f6
Leevi Uotinen <uotinen.leevi@hotmail.com>
2020-08-24 18:18:06 +0000

    Add devcontainer

--- duely-postgres/duely-reset.sql
+++ duely-postgres/duely-reset.sql
@@ -5,14 +5,40 @@
 \set QUIET true
 \set ON_ERROR_STOP true
 
-REVOKE CONNECT ON DATABASE duely FROM duely;
+DO 
+LANGUAGE plpgsql
+$_RESET_$
+BEGIN
 
-SELECT pg_terminate_backend(pid)
-FROM pg_stat_activity
-WHERE pid <> pg_backend_pid()
-  AND datname = 'duely';
+  IF NOT EXISTS (
+    SELECT 1
+    FROM pg_catalog.pg_roles
+    WHERE rolname = 'duely'
+  ) THEN
 
-DROP DATABASE duely;
+    CREATE ROLE duely LOGIN PASSWORD 'duely';
+
+  END IF;
+
+  IF EXISTS (
+    SELECT 1
+    FROM pg_catalog.pg_database
+    WHERE datname = 'duely'
+  ) THEN
+
+    REVOKE CONNECT ON DATABASE duely FROM duely;
+
+    PERFORM pg_terminate_backend(pid)
+    FROM pg_stat_activity
+    WHERE pid <> pg_backend_pid()
+      AND datname = 'duely';
+
+  END IF;
+
+END;
+$_RESET_$;
+
+DROP DATABASE IF EXISTS duely;
 CREATE DATABASE duely;
 
 \c duely
