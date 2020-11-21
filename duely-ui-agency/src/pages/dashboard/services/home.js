2597b27cb08e2064bb658a86766cfa2dbf5c3c20
Leevi Uotinen <uotinen.leevi@hotmail.com>
2020-11-21 22:50:13 +0000

    Enchance Table component by enabling wrapping on smaller screen while allowing some columns to stay on same row as other column.

--- duely-ui-agency/src/pages/dashboard/services/home.js
+++ duely-ui-agency/src/pages/dashboard/services/home.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import { useQuery, current_agency_Q } from '@duely/client';
-import { Table } from '@duely/react';
+import { Util, useBreakpoints, Table } from '@duely/react';
 import {
   DashboardFlexGrid,
   DashboardCard,
   DashboardCardGetStartedCreateServices,
   DashboardSection
 } from '../components';
-import { BsThreeDotsVertical } from 'react-icons/bs';
-
-function formatCurrency(amount, currency, country_code) {
-  currency = currency.toUpperCase();
-  country_code = country_code ?? 'US';
-  return new Intl.NumberFormat('en-' + country_code, {
-    currency,
-    style: 'currency',
-  }).format(amount);
-}
+import { BsPencilSquare, BsThreeDotsVertical, BsTrash } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 const statusChipClassNames = {
   draft: 'bg-orange-100 text-orange-800',
   live: 'bg-green-100 text-green-800'
 };
 
+const wrap = {
+  columns: 5,
+  spans: [
+    5,
+    2, 3
+  ]
+};
+
+const headers = [
+  'Service',
+  'Status',
+  'Action'
+];
+
 export default function DashboardServicesHome() {
   const { data: agency } = useQuery(current_agency_Q);
-
-  const headers = [
-    'Service',
-    'Status',
-    'Action'
-  ];
+  const { sm } = useBreakpoints();
 
   const rows = agency?.services.map(service => {
     const { default_variant } = service;
@@ -55,7 +56,7 @@ export default function DashboardServicesHome() {
           <span className="font-medium">{item.service.name}</span>
           <p className="text-xs text-gray-500 flex-1">{item.variant.description}</p>
           <div className="h-4 flex items-center space-x-2 text-gray-500 text-xs pb-3">{item.price && (
-            <span>{formatCurrency(item.price.unit_amount / 100, item.price.currency)}</span>
+            <span>{Util.formatCurrency(item.price.unit_amount / 100, item.price.currency)}</span>
           )}
           </div>
         </div>
@@ -70,11 +71,39 @@ export default function DashboardServicesHome() {
     ),
 
     // actions
-    item => (
-      <div className="flex text-xl font-medium text-gray-500 pr-1">
-        <BsThreeDotsVertical />
-      </div>
-    ),
+    item => {
+      const actions = [
+        {
+          key: 'edit',
+          children: (
+            <div className="flex items-center space-x-2 text-center text-sm text-gray-500 hover:text-accent">
+              <BsPencilSquare />
+              <span>Edit</span>
+            </div>
+          ),
+          to: '#edit/' + item.service.id
+        },
+        {
+          key: 'delete',
+          children: (
+            <div className="flex items-center space-x-2 text-center text-sm text-gray-500 hover:text-accent">
+              <BsTrash />
+              <span>Delete</span>
+            </div>
+          ),
+          to: '#delete/' + item.service.id
+        }
+      ];
+
+      return (
+        <div className="flex font-medium space-x-6">
+          {sm && <BsThreeDotsVertical className="text-xl text-gray-500" />}
+          {!sm && actions.map(action => (
+            <Link {...action} />
+          ))}
+        </div>
+      );
+    },
   ];
 
   return (
@@ -87,7 +116,7 @@ export default function DashboardServicesHome() {
 
       <DashboardSection title="Services">
         <DashboardCard>
-          <Table className="px-6 py-4" rows={rows} columns={columns} headers={headers} />
+          <Table className="px-6 py-4" rows={rows} columns={columns} headers={headers} wrap={wrap} />
         </DashboardCard>
       </DashboardSection>
     </>
