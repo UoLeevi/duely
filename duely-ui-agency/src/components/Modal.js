bff761aebec1a12a8446c8c196290e8557f65eda
Leevi Uotinen <uotinen.leevi@hotmail.com>
2020-11-26 17:16:31 +0000

    Add draft for confirm service deletion modal.

---
+++ duely-ui-agency/src/components/Modal.js
@@ -0,0 +1,53 @@
+import { Util, ScreenOverlayContext } from '@duely/react';
+import React, { useContext, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { Transition } from '@headlessui/react';
+import { BsX } from 'react-icons/bs';
+
+function ModalContent({ children, close, openerRef, className }) {
+  const ref = useRef();
+
+  useEffect(() => ref.current.focus(), []);
+
+  function onBlur(e) {
+    const container = ref.current;
+    const focusedEl = e.relatedTarget;
+    if (focusedEl === openerRef?.current)
+      return;
+    if (!focusedEl || !container.contains(focusedEl)) {
+      close?.();
+    }
+  }
+
+  className = Util.createClassName(className, 'flex flex-col relative shadow-lg box-border focus:outline-none');
+
+  return (
+    <div ref={ref} tabIndex="-1" className={className} onClick={e => e.stopPropagation()} onBlur={onBlur}>
+      {/* {close && <BsX onClick={close} className="absolute right-0 top-0 text-2xl m-3 text-gray-600" />} */}
+      {children}
+    </div>
+  );
+}
+
+export function Modal({ children, show, close, openerRef, className }) {
+  const screenOverlayRef = useContext(ScreenOverlayContext);
+
+  return ReactDOM.createPortal(
+    <Transition
+      show={show}
+      enter="transition ease-out duration-75"
+      enterFrom="transform opacity-0 scale-95"
+      enterTo="transform opacity-100 scale-100"
+      leave="transition ease-in duration-75"
+      leaveFrom="transform opacity-100 scale-100"
+      leaveTo="transform opacity-0 scale-95"
+    >
+      <div className="fixed left-0 top-0 w-full h-full z-30 flex items-center justify-center bg-gray-100 bg-opacity-75 bg-blur pointer-events-auto">
+        <ModalContent openerRef={openerRef} close={close} className={className}>
+          {children}
+        </ModalContent>
+      </div>
+    </Transition>,
+    screenOverlayRef.current
+  );
+}
--- duely-ui-agency/src/pages/dashboard/services/home.js
+++ duely-ui-agency/src/pages/dashboard/services/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useQuery, current_agency_Q } from '@duely/client';
 import { Util, useBreakpoints, Table, DropMenu, Card } from '@duely/react';
 import {
@@ -6,8 +6,10 @@ import {
   DashboardCardGetStartedCreateServices,
   DashboardSection
 } from '../components';
-import { BsPencilSquare, BsTrash } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { BsExclamationTriangle, BsPencilSquare, BsTrash } from 'react-icons/bs';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { Modal } from 'components/Modal';
+import produce from 'immer';
 
 const statusChipClassNames = {
   draft: 'bg-orange-100 text-orange-800',
@@ -27,6 +29,53 @@ const headers = [
   'Status',
   'Action'
 ];
+
+function ConfirmServiceDeletionModal() {
+  // Get service id from url query string and replace history entry if cancelled
+  const history = useHistory();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const service_id = searchParams.get('delete_service');
+  const show = service_id != null;
+
+  const { data: agency } = useQuery(current_agency_Q);
+
+  const service = service_id && agency?.services.find(service => service.id === service_id);
+
+  const close = useCallback(() => {
+    const searchParams = new URLSearchParams(history.location.search);
+    searchParams.delete('delete_service');
+
+    const location = produce(history.location, location => {
+      const search = '?' + searchParams.toString();
+      location.search = search === '?' ? '' : search;
+    });
+
+    history.replace(location);
+  }, [history]);
+
+  return (
+    <Modal show={show} className="max-w-lg rounded-lg">
+      <div className="flex bg-white p-6 space-x-4 rounded-t-lg">
+        <div className="w-10 h-10 grid place-items-center text-xl rounded-full bg-red-100 text-red-700">
+          <BsExclamationTriangle />
+        </div>
+        <div className="flex flex-col space-y-4 flex-1">
+          <span className="text-xl font-medium">Delete service</span>
+          <p className="text-gray-600 text-sm">Are you sure you want to delete service <span className="font-semibold">{service?.name}</span>? The service will be permanently removed. This action cannot be undone.</p>
+        </div>
+      </div>
+      <div className="flex bg-gray-50 px-6 py-4 space-x-4 rounded-b-lg">
+        <button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+          Delete
+        </button>
+        <button type="button" onClick={close} className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+          Cancel
+        </button>
+      </div>
+    </Modal>
+  );
+}
 
 export default function DashboardServicesHome() {
   const { data: agency } = useQuery(current_agency_Q);
@@ -92,7 +141,7 @@ export default function DashboardServicesHome() {
               <span>Delete</span>
             </div>
           ),
-          to: '#delete/' + item.service.id
+          to: '?delete_service=' + item.service.id
         }
       ];
 
@@ -123,6 +172,8 @@ export default function DashboardServicesHome() {
           <Table className="px-6 py-4" rows={rows} columns={columns} headers={headers} wrap={wrap} />
         </Card>
       </DashboardSection>
+
+      <ConfirmServiceDeletionModal />
     </>
   );
 }
