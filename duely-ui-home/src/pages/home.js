e318a137bf79883c597c2daf5db4ee35554458f5
Leevi Uotinen <uotinen.leevi@hotmail.com>
2020-12-22 10:23:54 +0000

    Update duely-ui-home dependencies.

--- duely-ui-home/src/pages/home.js
+++ duely-ui-home/src/pages/home.js
@@ -5,7 +5,7 @@ import { BsExclamationDiamond } from 'react-icons/bs';
 
 export default function Home() {
   useMessage(
-    <div className="flex flex-row items-center font-semibold space-x-4">
+    <div className="flex flex-row items-center space-x-4 font-semibold">
       <BsExclamationDiamond className="text-orange-600" />
       <span className="text-sm">Duely is still in development</span>
     </div>
@@ -17,13 +17,13 @@ export default function Home() {
       <div className="page-body-container">
         <main className="flex flex-col justify-around flex-grow flex-shrink-0 py-6">
           <div className="flex flex-col items-center">
-            <div className="font-bold text-gray-400 text-xl sm:text-2xl mb-2">Duely</div>
-            <h3 className="font-bold text-4xl sm:text-5xl md:text-6xl text-center">
+            <div className="mb-2 text-xl font-bold text-gray-400 sm:text-2xl">Duely</div>
+            <h3 className="text-4xl font-bold text-center sm:text-6xl md:text-7xl lg:text-8xl">
               <span>Platform for</span><br />
-              <span className="font-black bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-blue-500 to-green-500">digital agencies</span>
+              <span className="font-black text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-blue-500 to-green-500">digital agencies</span>
             </h3>
             <Link to="/sign-up">
-              <button className="mt-6 sm:mt-8 bg-indigo-500 hover:bg-indigo-700 px-5 py-3 rounded-md shadow-lg text-md font-medium leading-5 text-white transition duration-150 ease-in-out border border-gray-300 focus:outline-none focus:border-blue-300 focus:ring-blue active:bg-gray-50">Request early access</button>
+              <button className="px-5 py-3 mt-6 font-medium leading-5 text-white transition duration-150 ease-in-out bg-indigo-500 border border-gray-300 rounded-md shadow-lg sm:mt-8 hover:bg-indigo-700 text-md focus:outline-none focus:border-blue-300 focus:ring-blue active:bg-gray-50">Request early access</button>
             </Link>
           </div>
         </main>
