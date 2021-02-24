d3d842c78790db5f931836c5b38826ed9922e2a1
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-02-24 17:45:00 +0200

    Add line_items resolver for StripeCheckoutSession gql type.

--- duely-graphql/src/graphql/schema/StripeCheckoutSession/index.ts
+++ duely-graphql/src/graphql/schema/StripeCheckoutSession/index.ts
@@ -1,7 +1,10 @@
 // see: https://stripe.com/docs/api/checkout/sessions/object
 
+import { parseResolveInfo, ResolveTree } from 'graphql-parse-resolve-info';
 import gql from 'graphql-tag';
+import stripe from '../../../stripe';
 import { GqlTypeDefinition } from '../../types';
+import { withStripeAccountProperty } from '../../util';
 
 // TODO: define rest of fields
 /* 
@@ -48,7 +51,13 @@ export const StripeCheckoutSession: GqlTypeDefinition = {
       cancel_url: String
       client_reference_id: String
       currency: String
+      customer: StripeCustomer
       customer_email: String
+      line_items(
+        starting_after_id: String
+        ending_before_id: String
+        limit: Int
+      ): [LineItem!]!
       livemode: Boolean
       locale: String
       mode: String
@@ -57,10 +66,66 @@ export const StripeCheckoutSession: GqlTypeDefinition = {
       submit_type: String
       success_url: String
     }
+
+    type LineItem {
+      id: ID!
+      amount_subtotal: Int!
+      amount_total: Int!
+      currency: String!
+      description: String!
+      price: String
+      quantity: Int
+    }
   `,
   resolvers: {
     StripeCheckoutSession: {
-      id_ext: (source) => source.id
+      id_ext: (source) => source.id,
+      async customer(source, args, context, info) {
+        if (source.customer == null) return null;
+        if (typeof source.customer === 'object') return source.customer;
+
+        const stripe_env = source.livemode ? 'live' : 'test';
+
+        const resolveTree = parseResolveInfo(info) as ResolveTree;
+        const fields = Object.keys(Object.values(resolveTree.fieldsByTypeName)[0]);
+        if (fields.length === 1 && fields[0] === 'id') return { id: source.customer };
+
+        const customer = await stripe[stripe_env].customers.retrieve(source.customer, {
+          stripeAccount: source.stripeAccount
+        });
+        return withStripeAccountProperty(customer, source);
+      },
+      async line_items(
+          source,
+          { starting_after_id, ending_before_id, ...args },
+          context,
+          info
+        ) {
+          if (!context.jwt) throw new Error('Unauthorized');
+  
+          const stripe_env = source.livemode ? 'live' : 'test';
+  
+          try {
+            if (starting_after_id) {
+              args.starting_after = starting_after_id;
+            }
+  
+            if (ending_before_id) {
+              args.ending_before = ending_before_id;
+            }
+  
+            // see: https://stripe.com/docs/api/checkout/sessions/line_items
+            const list = await stripe[stripe_env].checkout.sessions.listLineItems(source.id, args, {
+              stripeAccount: source.stripe_id_ext
+            });
+            return list.data?.map((item) => ({
+              stripeAccount: source.stripe_id_ext,
+              ...item
+            }));
+          } catch (error) {
+            throw new Error(error.message);
+          }
+      }
     }
   }
 };
