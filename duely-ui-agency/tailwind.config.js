4f991e6b5097a9039a34658790ee4c975105843e
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-04-17 13:32:23 +0300

    Update tailwind.config.js.

--- duely-ui-agency/tailwind.config.js
+++ duely-ui-agency/tailwind.config.js
@@ -2,26 +2,29 @@ const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   plugins: [
-    plugin(function({ addVariant, e }) {
+    plugin(function ({ addVariant, e }) {
       addVariant('router-link-active', ({ modifySelectors, separator }) => {
         modifySelectors(({ className }) => {
-          return `.active .${e(`router-link-active${separator}${className}`)}`
-        })
-      })
+          return `.active .${e(`router-link-active${separator}${className}`)}`;
+        });
+      });
     })
   ],
-  presets: [
-    require('@duely/react/preset-duely')
-  ],
-  purge: [
-    './public/*.html',
-    './src/**/*.js',
-    './src/**/*.ts',
-    './src/**/*.tsx',
-    './node_modules/\\@duely/react/dist/**/*.js'
-  ],
+  presets: [require('@duely/react/preset-duely')],
+  purge: {
+    content: [
+      './public/*.html',
+      './src/**/*.js',
+      './src/**/*.ts',
+      './src/**/*.tsx',
+      './node_modules/\\@duely/react/dist/**/*.js'
+    ],
+    options: {
+      safelist: [/^grecaptcha-/]
+    }
+  },
   variants: {
     borderWidth: ['responsive', 'first', 'last'],
-    display: ['responsive', 'router-link-active'],
+    display: ['responsive', 'router-link-active']
   }
-}
+};
--- duely-ui-home/tailwind.config.js
+++ duely-ui-home/tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  presets: [
-    require('@duely/react/preset-duely')
-  ],
-  purge: [
-    './public/*.html',
-    './src/**/*.js',
-    './src/**/*.ts',
-    './src/**/*.tsx',
-    './node_modules/\\@duely/react/dist/**/*.js'
-  ]
-}
+  presets: [require('@duely/react/preset-duely')],
+  purge: {
+    content: [
+      './public/*.html',
+      './src/**/*.js',
+      './src/**/*.ts',
+      './src/**/*.tsx',
+      './node_modules/\\@duely/react/dist/**/*.js'
+    ],
+    options: {
+      safelist: [/^grecaptcha-/]
+    }
+  }
+};
