f98519a49f3725f2390d70a46df914b15f6d23f3
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-04-18 11:10:24 +0300

    Add teachable integration type form fields.

--- duely-postgres/duely-migrate.sql
+++ duely-postgres/duely-migrate.sql
@@ -14,30 +14,15 @@ DECLARE
 BEGIN
 -- MIGRATION CODE START
 
-CREATE OR REPLACE FUNCTION internal_.set_form_field_sort_key_() RETURNS trigger
-    LANGUAGE plpgsql
-    AS $$
-DECLARE
-  _sort_key real;
-BEGIN
-  SELECT MAX(s.sort_key_) + 1 INTO _sort_key
-  FROM internal_.form_field_ s
-  WHERE s.form_uuid_ = NEW.form_uuid_;
+INSERT INTO internal_.form_field_ (name_, type_, form_uuid_, default_, label_)
+SELECT 'school_domain', 'text', i.form_uuid_, NULL, 'School domain name'
+FROM internal_.integration_type_ i
+WHERE i.name_ = 'teachable/enroll';
 
-  NEW.sort_key_ = COALESCE(_sort_key, 0);
-
-  RETURN NEW;
-END
-$$;
-
-DROP TRIGGER tr_before_insert_set_sort_key_ ON internal_.form_field_;
-
-CREATE TRIGGER tr_before_insert_set_sort_key_ BEFORE INSERT ON internal_.form_field_ FOR EACH ROW WHEN (NEW.sort_key_ IS NULL) EXECUTE FUNCTION internal_.set_form_field_sort_key_();
-
-
-INSERT INTO internal_.form_ (uuid_) VALUES (DEFAULT) RETURNING uuid_ INTO _form_uuid;
-
-INSERT INTO internal_.integration_type_ (form_uuid_, name_) VALUES (_form_uuid, 'teachable/enroll');
+INSERT INTO internal_.form_field_ (name_, type_, form_uuid_, default_, label_)
+SELECT 'product_id', 'text', i.form_uuid_, NULL, 'Product ID'
+FROM internal_.integration_type_ i
+WHERE i.name_ = 'teachable/enroll';
 
 -- MIGRATION CODE END
 EXCEPTION WHEN OTHERS THEN
