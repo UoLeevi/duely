9e4a22f0f1eb8569ec39add445d0f43097014bdc
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-04-21 17:26:22 +0300

    Add resolvers for FormField type.

--- duely-graphql/src/graphql/schema/FormField/index.ts
+++ duely-graphql/src/graphql/schema/FormField/index.ts
@@ -1,5 +1,11 @@
 import gql from 'graphql-tag';
 import { GqlTypeDefinition } from '../../types';
+import { createDefaultQueryResolversForResource } from '../../util';
+
+const resource = {
+  name: 'form field',
+  table_name: 'form_field'
+} as const;
 
 export const FormField: GqlTypeDefinition = {
   typeDef: gql`
@@ -10,5 +16,19 @@ export const FormField: GqlTypeDefinition = {
       type: String!
       default: Json
     }
-  `
+
+    input FormFieldFilter {
+      form_id: ID
+    }
+
+    extend type Query {
+      form_field(id: ID!): FormField
+      form_fields(filter: FormFieldFilter!): [FormField!]
+    }
+  `,
+  resolvers: {
+    Query: {
+      ...createDefaultQueryResolversForResource(resource)
+    }
+  }
 };
