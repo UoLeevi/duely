0f4d2afceeba52e93a1e81f391cd5c92b0d802bf
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-07-15 13:22:20 +0300

    Add product edit form section for updating integration options.

---
+++ duely-ui-agency/src/pages/dashboard/products/[product_url_name]/components/UpdateProductIntegrationForm.tsx
@@ -0,0 +1,114 @@
+import {
+  integrations_Q,
+  integration_type_Q,
+  product_Q,
+  update_integration_M,
+  useMutation,
+  useQuery
+} from '@duely/client';
+import {
+  Form,
+  FormButton,
+  FormInfoMessage,
+  useFormMessages,
+  useForm,
+  DynamicFormFields
+} from '@duely/react';
+
+import { Util as CoreUtil } from '@duely/core';
+import { useMemo } from 'react';
+
+type ProductProps = {
+  product_id?: string;
+};
+
+type UpdateProductIntegrationFormFields = Record<string, string>;
+
+export function UpdateProductIntegrationForm({ product_id }: ProductProps) {
+  const form = useForm<UpdateProductIntegrationFormFields>();
+  const { data: product, loading: productLoading } = useQuery(
+    product_Q,
+    { product_id: product_id! },
+    { skip: !product_id }
+  );
+  const [updateIntegration, stateUpdate] = useMutation(update_integration_M);
+  const {
+    infoMessage,
+    setInfoMessage,
+    successMessage,
+    setSuccessMessage,
+    errorMessage,
+    setErrorMessage
+  } = useFormMessages();
+
+  const state = {
+    loading: !product_id || productLoading
+  };
+
+  const { data: integrations, loading: integrationsLoading } = useQuery(integrations_Q, {
+    filter: {
+      product_id
+    }
+  });
+
+  const integration = integrations?.[0];
+
+  const integration_values = useMemo(
+    () => JSON.parse(integration?.data ?? null),
+    [integration?.data]
+  );
+
+  const { data: integration_type, loading: integration_typeLoading } = useQuery(
+    integration_type_Q,
+    {
+      integration_type_id: integration?.integration_type?.id!
+    },
+    {
+      skip: !integration
+    }
+  );
+
+  async function onSubmit({ ...data }: UpdateProductIntegrationFormFields) {
+    const integration_json = JSON.stringify(
+      CoreUtil.pick(
+        data,
+        integration_type!.fields!.map((f) => f.name)
+      )
+    );
+
+    const res = await updateIntegration({
+      integration_id: integration!.id,
+      data: integration_json
+    });
+
+    if (res?.success) {
+      setSuccessMessage('Saved');
+      return;
+    } else {
+      setErrorMessage(res?.message ?? 'Unable to save changes. Something went wrong.');
+    }
+  }
+
+  return (
+    <>
+      <Form form={form} onSubmit={onSubmit} className="flex flex-col space-y-3">
+        <DynamicFormFields
+          loading={integration_typeLoading || integrationsLoading}
+          fields={integration_type?.fields ?? []}
+          defaultValues={integration_values}
+          skeletonFieldCount={1}
+        />
+
+        <div className="flex flex-row items-center pt-3 space-x-4">
+          <FormButton dense disabled={stateUpdate.loading}>
+            Save
+          </FormButton>
+          <FormButton type="reset" dense disabled={stateUpdate.loading}>
+            Cancel
+          </FormButton>
+          <FormInfoMessage error={errorMessage} info={infoMessage} success={successMessage} />
+        </div>
+      </Form>
+    </>
+  );
+}
--- duely-ui-agency/src/pages/dashboard/products/[product_url_name]/edit.tsx
+++ duely-ui-agency/src/pages/dashboard/products/[product_url_name]/edit.tsx
@@ -1,9 +1,17 @@
-import { current_subdomain_Q, product_and_agency_from_url_parts_Q, useQuery } from '@duely/client';
+import {
+  current_subdomain_Q,
+  integrations_Q,
+  integration_config_Q,
+  integration_type_Q,
+  product_and_agency_from_url_parts_Q,
+  useQuery
+} from '@duely/client';
 import { Card, FormSection } from '@duely/react';
 import { Link, useParams } from 'react-router-dom';
 import { DashboardSection } from '../../components';
 import { UpdateProductBasicInfoForm } from './components';
 import { UpdateProductCheckoutSettingsForm } from './components/UpdateProductCheckoutSettingsForm';
+import { UpdateProductIntegrationForm } from './components/UpdateProductIntegrationForm';
 import { UpdateProductPricingForm } from './components/UpdateProductPricingForm';
 import { UpdateProductStatusForm } from './components/UpdateProductStatusForm';
 
@@ -15,6 +23,30 @@ export default function DashboardProductsEditProduct() {
     subdomain_name: current_subdomain?.name!,
     product_url_name
   });
+
+  const { data: integrations, loading: integrationsLoading } = useQuery(
+    integrations_Q,
+    {
+      filter: {
+        product_id: product?.id!
+      }
+    },
+    {
+      skip: !product
+    }
+  );
+
+  const integration = integrations?.[0];
+
+  const { data: integration_config, loading: integration_configLoading } = useQuery(
+    integration_config_Q,
+    {
+      integration_config_id: integration?.integration_config?.id!
+    },
+    {
+      skip: !integration
+    }
+  );
 
   return (
     <>
@@ -36,6 +68,17 @@ export default function DashboardProductsEditProduct() {
             <UpdateProductPricingForm product_id={product?.id} />
           </FormSection>
         </Card>
+
+        {integration_config && (
+          <Card>
+            <FormSection
+              title={`Integration - ${integration_config.name}`}
+              description="Configure settings for integration with external service."
+            >
+              <UpdateProductIntegrationForm product_id={product?.id} />
+            </FormSection>
+          </Card>
+        )}
 
         <Card>
           <FormSection
