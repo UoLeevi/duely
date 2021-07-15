dc3a2d099c47e9f40b996abad6719f1394c89ab0
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-07-15 12:30:12 +0300

    Improve error logging in integration/teachable/enroll lambda.

--- duely-lambda/src/jobs/integration/teachable/enroll/index.ts
+++ duely-lambda/src/jobs/integration/teachable/enroll/index.ts
@@ -88,7 +88,21 @@ async function main() {
       );
     });
   } catch (err: any) {
-    console.error(`integration/teachable/enroll failed:\n${err}`);
+    console.error(`integration/teachable/enroll failed.`);
+
+    if (err.response) {
+      // Request made and server responded
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+    } else if (err.request) {
+      // The request was made but no response was received
+      console.log(err.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', err.message);
+    }
+
     throw err;
   }
 }
