c4eb7efce4e37c44114661d02a617c7ff4d040fa
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-09-30 18:45:57 +0300

    Add stripe endpoint types.

--- @duely/stripe/src/index.ts
+++ @duely/stripe/src/index.ts
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 import fs from 'fs';
 import { ResourceId, Resources } from '@duely/db';
+import { SplitString, Awaited, FilterKeys, PathValue } from '@duely/util';
 
 const STRIPE_API_VERSION = '2020-08-27';
 
@@ -20,17 +21,17 @@ if (!process.env.STRIPE_SK_TEST || !process.env.STRIPE_SK_LIVE) {
   }
 }
 
-const connectedAccounts = { 
+const connectedAccounts = {
   test: new Map<ResourceId<'stripe account'>, Stripe>(),
   live: new Map<ResourceId<'stripe account'>, Stripe>()
 };
 
 const stripe = {
   test: new Stripe(process.env.STRIPE_SK_TEST, {
-    apiVersion: STRIPE_API_VERSION,
+    apiVersion: STRIPE_API_VERSION
   }),
   live: new Stripe(process.env.STRIPE_SK_LIVE, {
-    apiVersion: STRIPE_API_VERSION,
+    apiVersion: STRIPE_API_VERSION
   }),
   get(stripe_account: Resources['stripe account'], livemode?: boolean): Stripe {
     livemode = livemode ?? stripe_account.livemode;
@@ -50,5 +51,44 @@ const stripe = {
     return stripe;
   }
 };
+
+type StripeResourceEndpointPathL1 = FilterKeys<Stripe, Record<'retrieve', (...args: any) => any>>;
+type StripeResourceEndpointPathL2 = FilterKeys<
+  Stripe,
+  Record<string, Record<'retrieve', (...args: any) => any>>
+> extends infer K
+  ? K extends string & keyof Stripe
+    ? FilterKeys<Stripe[K], Record<'retrieve', (...args: any) => any>> extends infer E
+      ? E extends string
+        ? `${K}.${E}`
+        : never
+      : never
+    : never
+  : never;
+
+type StripeResourceEndpointPath = StripeResourceEndpointPathL1 | StripeResourceEndpointPathL2;
+
+export type StripeResourceEndpoint = {
+  [Path in StripeResourceEndpointPath]: PathValue<Stripe, SplitString<Path, '.'>>;
+};
+
+type ExtractObjectFromStripeResponse<T extends Stripe.Response<unknown>> =
+  T extends Stripe.Response<infer O> ? O : never;
+
+export type StripeResourceEndpointResponseObject = {
+  [Path in keyof StripeResourceEndpoint]: ExtractObjectFromStripeResponse<
+    Awaited<ReturnType<StripeResourceEndpoint[Path]['retrieve']>>
+  >;
+};
+
+export type StripeDeletableResourceEndpoint = {
+  [Path in FilterKeys<
+    StripeResourceEndpoint,
+    Record<'del', (...args: any) => any>
+  >]: StripeResourceEndpoint[Path];
+};
+
+export type StripeDeletableObjectType =
+  StripeResourceEndpointResponseObject[keyof StripeDeletableResourceEndpoint]['object'];
 
 export default stripe;
