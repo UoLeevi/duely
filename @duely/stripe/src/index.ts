d4b07676382ecbf083adc6e0fbfd4f5383db0eb7
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-10-01 19:54:29 +0300

    Add stripe utility functions and types.

--- @duely/stripe/src/index.ts
+++ @duely/stripe/src/index.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 import fs from 'fs';
 import { ResourceId, Resources } from '@duely/db';
-import { SplitString, Awaited, FilterKeys, PathValue } from '@duely/util';
+import { SplitString, Awaited, FilterKeys, PathValue, get } from '@duely/util';
 
 const STRIPE_API_VERSION = '2020-08-27';
 
@@ -88,7 +88,88 @@ export type StripeDeletableResourceEndpoint = {
   >]: StripeResourceEndpoint[Path];
 };
 
+export type StripeObjectType =
+  StripeResourceEndpointResponseObject[keyof StripeResourceEndpoint]['object'];
+
 export type StripeDeletableObjectType =
   StripeResourceEndpointResponseObject[keyof StripeDeletableResourceEndpoint]['object'];
+
+const stripeResourceEndpointByType: {
+  [ObjectType in StripeObjectType]: FilterKeys<
+    StripeResourceEndpointResponseObject,
+    Record<'object', ObjectType>
+  >;
+} = {
+  product: 'products',
+  customer: 'customers',
+  price: 'prices',
+  order: 'orders',
+  balance: 'balance',
+  account: 'accounts',
+  apple_pay_domain: 'applePayDomains',
+  application_fee: 'applicationFees',
+  balance_transaction: 'balanceTransactions',
+  charge: 'charges',
+  country_spec: 'countrySpecs',
+  coupon: 'coupons',
+  credit_note: 'creditNotes',
+  dispute: 'disputes',
+  event: 'events',
+  exchange_rate: 'exchangeRates',
+  file: 'files',
+  file_link: 'fileLinks',
+  invoice: 'invoices',
+  invoiceitem: 'invoiceItems',
+  issuer_fraud_record: 'issuerFraudRecords',
+  mandate: 'mandates',
+  order_return: 'orderReturns',
+  payment_intent: 'paymentIntents',
+  payment_method: 'paymentMethods',
+  payout: 'payouts',
+  plan: 'plans',
+  promotion_code: 'promotionCodes',
+  quote: 'quotes',
+  refund: 'refunds',
+  review: 'reviews',
+  setup_intent: 'setupIntents',
+  sku: 'skus',
+  source: 'sources',
+  subscription: 'subscriptions',
+  subscription_item: 'subscriptionItems',
+  subscription_schedule: 'subscriptionSchedules',
+  tax_code: 'taxCodes',
+  tax_rate: 'taxRates',
+  token: 'tokens',
+  topup: 'topups',
+  transfer: 'transfers',
+  webhook_endpoint: 'webhookEndpoints',
+  'checkout.session': 'checkout.sessions',
+  'identity.verification_report': 'identity.verificationReports',
+  'identity.verification_session': 'identity.verificationSessions',
+  'issuing.authorization': 'issuing.authorizations',
+  'issuing.card': 'issuing.cards',
+  'issuing.cardholder': 'issuing.cardholders',
+  'issuing.dispute': 'issuing.disputes',
+  'issuing.transaction': 'issuing.transactions',
+  'radar.early_fraud_warning': 'radar.earlyFraudWarnings',
+  'radar.value_list': 'radar.valueLists',
+  'radar.value_list_item': 'radar.valueListItems',
+  'reporting.report_run': 'reporting.reportRuns',
+  'reporting.report_type': 'reporting.reportTypes',
+  scheduled_query_run: 'sigma.scheduledQueryRuns'
+};
+
+export function getStripeResourceForObjectType<
+  ObjectType extends keyof typeof stripeResourceEndpointByType
+>(
+  stripe: Stripe,
+  objectType: ObjectType
+): PathValue<
+  Stripe,
+  SplitString<FilterKeys<StripeResourceEndpointResponseObject, Record<'object', ObjectType>>, '.'>
+> {
+  const endpoint = stripeResourceEndpointByType[objectType];
+  return get(stripe, endpoint) as any;
+}
 
 export default stripe;
