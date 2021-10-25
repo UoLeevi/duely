10d8db60d55dbbb0522c59b701f59d940f23f1f2
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-10-25 17:07:47 +0300

    Add recurring_iterations_ field to price resource to emulate installment plans.

--- duely-postgres/duely-migrate.sql
+++ duely-postgres/duely-migrate.sql
@@ -15,104 +15,36 @@ DECLARE
 BEGIN
 -- MIGRATION CODE START
 
-CREATE OR REPLACE FUNCTION operation_.query_resource_(_resource_name text, _filter jsonb, _token text, _desc boolean DEFAULT false, _order_by text DEFAULT NULL::text, _limit integer DEFAULT NULL::integer, _offset integer DEFAULT NULL::integer, _before_id text DEFAULT NULL::text, _after_id text DEFAULT NULL::text) RETURNS SETOF jsonb
+CALL internal_.drop_auditing_('application_.price_');
+ALTER TABLE application_.price_ ADD COLUMN recurring_iterations_ integer NULL;
+CALL internal_.setup_auditing_('application_.price_');
+
+CREATE OR REPLACE FUNCTION policy_.agent_can_query_price_(_resource_definition security_.resource_definition_, _resource application_.resource_) RETURNS text[]
     LANGUAGE plpgsql SECURITY DEFINER
-    AS $_$
-DECLARE
-  _desc_asc text;
-  _lt_gt_before text;
-  _lt_gt_after text;
-  _order_by_sql text;
-  _filter_keys text[];
-  _table regclass;
+    AS $$
 BEGIN
-  IF _resource_name IS NULL OR _filter IS NULL OR _token IS NULL THEN
-    RETURN;
-  END IF;
-
-  _desc_asc := CASE WHEN _desc THEN 'DESC' ELSE 'ASC' END;
-  _lt_gt_before := CASE WHEN _desc THEN '<' ELSE '>' END;
-  _lt_gt_after := CASE WHEN _desc THEN '>' ELSE '<' END;
-
-  SELECT table_ INTO _table
-  FROM security_.resource_definition_
-  WHERE name_ = _resource_name;
-
-  IF _order_by IS NULL THEN
-    IF EXISTS (
-      SELECT 1
-      FROM pg_attribute
-      WHERE attrelid = _table
-        AND NOT attisdropped
-        AND attnum > 0
-        AND attname = 'sort_key_'
-    ) THEN
-      _order_by := 'sort_key_';
-    ELSE
-      _order_by := 'uuid_';
-    END IF;
+  IF internal_.check_resource_role_(_resource_definition, _resource, 'agent') THEN
+    RETURN '{uuid_, product_uuid_, stripe_price_id_ext_live_, stripe_price_id_ext_test_, type_, unit_amount_, currency_, recurring_interval_, recurring_interval_count_, recurring_iterations_, status_, active_}'::text[];
   ELSE
-    -- Convert to internal format
-    _order_by := CASE 
-      WHEN _order_by = 'id' THEN 'uuid_'
-      WHEN right(_order_by, 3) = '_id' THEN overlay(_order_by placing 'uuid_' from char_length(_order_by) - 1 for 2)
-      ELSE _order_by || '_'
-    END;
+    RETURN '{}'::text[];
   END IF;
-
-  _order_by_sql := format('ORDER BY t.%1$I %2$s, r.id_', _order_by, _desc_asc);
-
-  _filter := internal_.convert_to_internal_format_(_filter);
-
-  SELECT array_agg(keys_) INTO _filter_keys
-  FROM jsonb_object_keys(_filter) keys_;
-
-  RETURN QUERY EXECUTE format('
-    WITH
-      after_ AS (
-        SELECT r.id_, t.%1$I
-        FROM ' || _table || ' t
-        JOIN application_.resource_ r ON r.uuid_ = t.uuid_
-        JOIN security_.resource_definition_ d ON d.uuid_ = r.definition_uuid_
-        WHERE $4 IS NOT NULL
-          AND d.table_ = $1
-          AND r.id_ = $4
-      ),
-      before_ AS (
-        SELECT r.id_, t.%1$I
-        FROM ' || _table || ' t
-        JOIN application_.resource_ r ON r.uuid_ = t.uuid_
-        JOIN security_.resource_definition_ d ON d.uuid_ = r.definition_uuid_
-        WHERE $5 IS NOT NULL
-          AND d.table_ = $1
-          AND r.id_ = $5
-      ),
-      all_ AS (
-        SELECT internal_.dynamic_select_(d.table_, r.uuid_, x.keys_) data_
-        FROM ' || _table || ' t
-        JOIN application_.resource_ r ON r.uuid_ = t.uuid_
-        JOIN security_.resource_definition_ d ON d.uuid_ = r.definition_uuid_
-        JOIN security_.resource_token_ x ON r.uuid_ = x.resource_uuid_
-        LEFT JOIN after_ ON 1=1
-        LEFT JOIN before_ ON 1=1
-        WHERE d.table_ = $1
-          AND x.token_ = $6
-          AND r.search_ @> $2
-          AND x.keys_ @> $7
-          AND ($4 IS NULL OR t.%1$I %3$s after_.%1$I OR (t.%1$I %3$s= after_.%1$I AND r.id_ < after_.id_))
-          AND ($5 IS NULL OR t.%1$I %2$s before_.%1$I OR (t.%1$I %2$s= before_.%1$I AND r.id_ > before_.id_))
-        ' || _order_by_sql || '
-        LIMIT $3 OFFSET $8
-      )
-    SELECT internal_.convert_from_internal_format_(all_.data_) query_resource_
-    FROM all_;
-  ', _order_by, _lt_gt_before, _lt_gt_after)
-  USING _table, _filter, _limit, _before_id, _after_id, _token, _filter_keys, _offset;
 END
-$_$;
+$$;
 
-
-PERFORM security_.register_policy_('application_.stripe_account_', 'query', 'policy_.anyone_can_query_stripe_account_for_agency_');
+CREATE OR REPLACE FUNCTION policy_.owner_can_create_price_(_resource_definition security_.resource_definition_, _data jsonb) RETURNS text[]
+    LANGUAGE plpgsql SECURITY DEFINER
+    AS $$
+BEGIN
+  IF (
+    SELECT internal_.check_resource_role_(resource_definition_, resource_, 'owner')
+    FROM internal_.query_owner_resource_(_resource_definition, _data)
+  ) THEN
+    RETURN '{product_uuid_, stripe_price_id_ext_live_, stripe_price_id_ext_test_, unit_amount_, currency_, recurring_interval_, recurring_interval_count_, recurring_iterations_, status_, active_}'::text[];
+  ELSE
+    RETURN '{}'::text[];
+  END IF;
+END
+$$;
 
 -- MIGRATION CODE END
 EXCEPTION WHEN OTHERS THEN
