c231afb0f6777bc48bda6b352a3b47bab237233f
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-11-01 19:00:21 +0200

    Set product and price resource ids match corresponding stripe ids.

--- duely-postgres/duely-migrate.sql
+++ duely-postgres/duely-migrate.sql
@@ -15,106 +15,94 @@ DECLARE
 BEGIN
 -- MIGRATION CODE START
 
-CALL internal_.drop_auditing_('application_.product_');
-
-ALTER TABLE application_.product_ ADD COLUMN id_ text NULL;
-
-UPDATE application_.product_
-SET id_ = COALESCE(stripe_prod_id_ext_live_, stripe_prod_id_ext_test_);
-
-ALTER TABLE application_.product_ ALTER COLUMN id_ SET NOT NULL;
-ALTER TABLE application_.product_ ADD UNIQUE (id_);
+UPDATE application_.resource_ r
+SET id_ = p.id_
+FROM application_.price_ p
+WHERE p.uuid_ = r.uuid_;
 
 UPDATE application_.resource_ r
 SET id_ = p.id_
 FROM application_.product_ p
-CROSS JOIN security_.resource_definition_ d
-WHERE p.uuid_ = r.uuid_
-  AND d.uuid_ = r.definition_uuid_;
+WHERE p.uuid_ = r.uuid_;
 
-ALTER TABLE application_.product_
-    ADD FOREIGN KEY (id_) REFERENCES application_.resource_(id_) DEFERRABLE INITIALLY DEFERRED;
+-- CREATE OR REPLACE FUNCTION policy_.agent_can_query_product_(_resource_definition security_.resource_definition_, _resource application_.resource_) RETURNS text[]
+--     LANGUAGE plpgsql SECURITY DEFINER
+--     AS $$
+-- BEGIN
+--   IF internal_.check_resource_role_(_resource_definition, _resource, 'agent') THEN
+--     RETURN '{uuid_, id_, agency_uuid_, stripe_prod_id_ext_live_, stripe_prod_id_ext_test_, name_, url_name_, status_, active_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
+--   ELSE
+--     RETURN '{}'::text[];
+--   END IF;
+-- END
+-- $$;
 
-CALL internal_.setup_auditing_('application_.product_');
+-- CREATE OR REPLACE FUNCTION policy_.anyone_can_query_live_product_(_resource_definition security_.resource_definition_, _resource application_.resource_) RETURNS text[]
+--     LANGUAGE plpgsql SECURITY DEFINER
+--     AS $$
+-- BEGIN
+--   IF EXISTS (
+--     SELECT 1
+--     FROM application_.product_
+--     WHERE uuid_ = _resource.uuid_
+--       AND status_ = 'live'
+--       AND active_ = 't'
+--   ) THEN
+--     RETURN '{uuid_, id_, agency_uuid_, stripe_prod_id_ext_live_, stripe_prod_id_ext_test_, name_, url_name_, status_, active_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
+--   ELSE
+--     RETURN '{}'::text[];
+--   END IF;
+-- END
+-- $$;
 
-CREATE OR REPLACE FUNCTION policy_.agent_can_query_product_(_resource_definition security_.resource_definition_, _resource application_.resource_) RETURNS text[]
-    LANGUAGE plpgsql SECURITY DEFINER
-    AS $$
-BEGIN
-  IF internal_.check_resource_role_(_resource_definition, _resource, 'agent') THEN
-    RETURN '{uuid_, id_, agency_uuid_, stripe_prod_id_ext_live_, stripe_prod_id_ext_test_, name_, url_name_, status_, active_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
-  ELSE
-    RETURN '{}'::text[];
-  END IF;
-END
-$$;
+-- CREATE OR REPLACE FUNCTION policy_.owner_can_change_product_(_resource_definition security_.resource_definition_, _resource application_.resource_, _data jsonb) RETURNS text[]
+--     LANGUAGE plpgsql SECURITY DEFINER
+--     AS $$
+-- BEGIN
+--   IF internal_.check_resource_role_(_resource_definition, _resource, 'owner') THEN
+--     IF EXISTS (
+--       SELECT 1
+--       FROM application_.product_
+--       WHERE uuid_ = _resource.uuid_
+--         AND stripe_prod_id_ext_live_ IS NULL
+--         AND stripe_prod_id_ext_test_ IS NULL
+--     ) THEN
+--       RETURN '{name_, stripe_prod_id_ext_live_, stripe_prod_id_ext_test_, url_name_, status_, active_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
+--     ELSE
+--       RETURN '{name_, url_name_, status_, active_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
+--     END IF;
+--   ELSE
+--     RETURN '{}'::text[];
+--   END IF;
+-- END
+-- $$;
 
-CREATE OR REPLACE FUNCTION policy_.anyone_can_query_live_product_(_resource_definition security_.resource_definition_, _resource application_.resource_) RETURNS text[]
-    LANGUAGE plpgsql SECURITY DEFINER
-    AS $$
-BEGIN
-  IF EXISTS (
-    SELECT 1
-    FROM application_.product_
-    WHERE uuid_ = _resource.uuid_
-      AND status_ = 'live'
-      AND active_ = 't'
-  ) THEN
-    RETURN '{uuid_, id_, agency_uuid_, stripe_prod_id_ext_live_, stripe_prod_id_ext_test_, name_, url_name_, status_, active_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
-  ELSE
-    RETURN '{}'::text[];
-  END IF;
-END
-$$;
+-- CREATE OR REPLACE FUNCTION policy_.owner_can_create_product_(_resource_definition security_.resource_definition_, _data jsonb) RETURNS text[]
+--     LANGUAGE plpgsql SECURITY DEFINER
+--     AS $$
+-- BEGIN
+--   IF (
+--     SELECT internal_.check_resource_role_(resource_definition_, resource_, 'owner')
+--     FROM internal_.query_owner_resource_(_resource_definition, _data)
+--   ) THEN
+--     RETURN '{agency_uuid_, id_, stripe_prod_id_ext_live_, stripe_prod_id_ext_test_, name_, url_name_, status_, active_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
+--   ELSE
+--     RETURN '{}'::text[];
+--   END IF;
+-- END
+-- $$;
 
-CREATE OR REPLACE FUNCTION policy_.owner_can_change_product_(_resource_definition security_.resource_definition_, _resource application_.resource_, _data jsonb) RETURNS text[]
-    LANGUAGE plpgsql SECURITY DEFINER
-    AS $$
-BEGIN
-  IF internal_.check_resource_role_(_resource_definition, _resource, 'owner') THEN
-    IF EXISTS (
-      SELECT 1
-      FROM application_.product_
-      WHERE uuid_ = _resource.uuid_
-        AND stripe_prod_id_ext_live_ IS NULL
-        AND stripe_prod_id_ext_test_ IS NULL
-    ) THEN
-      RETURN '{name_, stripe_prod_id_ext_live_, stripe_prod_id_ext_test_, url_name_, status_, active_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
-    ELSE
-      RETURN '{name_, url_name_, status_, active_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
-    END IF;
-  ELSE
-    RETURN '{}'::text[];
-  END IF;
-END
-$$;
-
-CREATE OR REPLACE FUNCTION policy_.owner_can_create_product_(_resource_definition security_.resource_definition_, _data jsonb) RETURNS text[]
-    LANGUAGE plpgsql SECURITY DEFINER
-    AS $$
-BEGIN
-  IF (
-    SELECT internal_.check_resource_role_(resource_definition_, resource_, 'owner')
-    FROM internal_.query_owner_resource_(_resource_definition, _data)
-  ) THEN
-    RETURN '{agency_uuid_, id_, stripe_prod_id_ext_live_, stripe_prod_id_ext_test_, name_, url_name_, status_, active_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
-  ELSE
-    RETURN '{}'::text[];
-  END IF;
-END
-$$;
-
-CREATE OR REPLACE FUNCTION policy_.serviceaccount_can_query_product_(_resource_definition security_.resource_definition_, _resource application_.resource_) RETURNS text[]
-    LANGUAGE plpgsql SECURITY DEFINER
-    AS $$
-BEGIN
-  IF internal_.check_current_user_is_serviceaccount_() THEN
-    RETURN '{uuid_, id_, agency_uuid_, stripe_prod_id_ext_live_, stripe_prod_id_ext_test_, name_, url_name_, status_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
-  ELSE
-    RETURN '{}'::text[];
-  END IF;
-END
-$$;
+-- CREATE OR REPLACE FUNCTION policy_.serviceaccount_can_query_product_(_resource_definition security_.resource_definition_, _resource application_.resource_) RETURNS text[]
+--     LANGUAGE plpgsql SECURITY DEFINER
+--     AS $$
+-- BEGIN
+--   IF internal_.check_current_user_is_serviceaccount_() THEN
+--     RETURN '{uuid_, id_, agency_uuid_, stripe_prod_id_ext_live_, stripe_prod_id_ext_test_, name_, url_name_, status_, description_, duration_, default_price_uuid_, markdown_description_uuid_, image_logo_uuid_, image_hero_uuid_}'::text[];
+--   ELSE
+--     RETURN '{}'::text[];
+--   END IF;
+-- END
+-- $$;
 
 
 
