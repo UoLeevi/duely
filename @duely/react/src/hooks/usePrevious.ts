1deeb411e4fc64c38dc413c0193ff62359f49552
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-12-01 18:50:06 +0200

    Improve usePrevious hook.

--- @duely/react/src/hooks/usePrevious.ts
+++ @duely/react/src/hooks/usePrevious.ts
@@ -1,19 +1,48 @@
 import { useRef, useEffect } from 'react';
 
-const defaultOptions = { skip: false, retain: false };
+const defaultOptions = { skip: false, retain: false, returnTuple: false };
 
 export function usePrevious<T>(
   value: T,
-  options: { skip?: boolean; retain?: boolean } = defaultOptions
+  options: {
+    skip?: boolean | ((previous?: T, value?: T) => boolean);
+    retain?: boolean;
+    returnTuple?: false;
+  }
+): T;
+export function usePrevious<T>(
+  value: T,
+  options: {
+    skip?: boolean | ((previous?: T, value?: T) => boolean);
+    retain?: boolean;
+    returnTuple: true;
+  }
+): [T, { changed: boolean }];
+export function usePrevious<T>(
+  value: T,
+  options: {
+    skip?: boolean | ((previous?: T, value?: T) => boolean);
+    retain?: boolean;
+    returnTuple?: boolean;
+  } = defaultOptions
 ) {
   const ref = useRef(value);
+  const previous = ref.current;
+  const { skip, retain, returnTuple } = { ...defaultOptions, ...options };
 
-  const { skip, retain } = { ...defaultOptions, ...options };
-  useEffect(() => {
-    if (retain && value == null) return;
-    if (skip) return;
-    ref.current = value;
-  }, [value, skip, retain]);
+  // If value is equal to previous value, return previous value.
+  if (value === previous) return returnTuple ? [previous, { changed: false }] : previous;
+  
+  // If value is null or undefined and should be retained, return previous value.
+  if (retain && value == null) return returnTuple ? [previous, { changed: false }] : previous;
+  
+  // If value change should be skipped, return previous value.
+  if (typeof skip === 'function' ? skip(previous, value) : skip)
+    return returnTuple ? [previous, { changed: false }] : previous;
 
-  return ref.current;
+  // update previous value
+  ref.current = value;
+
+  // return previous value
+  return returnTuple ? [previous, { changed: true }] : previous;
 }
