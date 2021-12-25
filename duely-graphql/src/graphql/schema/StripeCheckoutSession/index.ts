3d73ee26fa1ae859a7602363abc5e4e11f732477
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-12-25 09:44:57 +0200

    Extend stripe checkout session gql definition.

--- duely-graphql/src/graphql/schema/StripeCheckoutSession/index.ts
+++ duely-graphql/src/graphql/schema/StripeCheckoutSession/index.ts
@@ -18,19 +18,19 @@ export const StripeCheckoutSession: GqlTypeDefinition<
   typeDef: gql`
     type StripeCheckoutSession {
       id: ID!
-      # after_expiration: Session.AfterExpiration | null;
+      after_expiration: SessionAfterExpiration
       allow_promotion_codes: Boolean
       amount_subtotal: Int
       amount_total: Int
-      # automatic_tax: Session.AutomaticTax;
+      automatic_tax: SessionAutomaticTax
       billing_address_collection: String
       cancel_url: String
       client_reference_id: String
-      # consent: Session.Consent | null;
-      # consent_collection: Session.ConsentCollection | null;
+      consent: SessionConsent
+      consent_collection: SessionConsentCollection
       currency: String
       customer: StripeCustomer
-      # customer_details: Session.CustomerDetails | null;
+      customer_details: SessionCustomerDetails
       customer_email: String
       expires_at: DateTime
       line_items(starting_after: String, ending_before: String, limit: Int): [LineItem!]!
@@ -41,19 +41,101 @@ export const StripeCheckoutSession: GqlTypeDefinition<
       payment_intent: PaymentIntent
       payment_method_types: [String]
       payment_status: String
-      # phone_number_collection?: Session.PhoneNumberCollection;
+      phone_number_collection: SessionPhoneNumberCollection
       # setup_intent: string | Stripe.SetupIntent | null;
-      # shipping: Session.Shipping | null;
-      # shipping_address_collection: Session.ShippingAddressCollection | null;
-      # shipping_options: Array<Session.ShippingOption>;
+      shipping: Shipping
+      shipping_address_collection: SessionShippingAddressCollection
+      shipping_options: [SessionShippingOption!]!
       # shipping_rate: string | Stripe.ShippingRate | null;
       status: String
       submit_type: String
       subscription: StripeSubscription
       success_url: String!
-      # tax_id_collection?: Session.TaxIdCollection;
-      # total_details: Session.TotalDetails | null;
+      tax_id_collection: SessionTaxIdCollection
+      total_details: SessionTotalDetails
       url: String
+    }
+
+    type SessionAfterExpiration {
+      recovery: SessionAfterExpirationRecovery
+    }
+
+    type SessionAfterExpirationRecovery {
+      allow_promotion_codes: Boolean
+      expires_at: DateTime
+      url: String
+    }
+
+    type SessionAutomaticTax {
+      enabled: Boolean!
+      status: String
+    }
+
+    type SessionConsent {
+      promotions: String
+    }
+
+    type SessionConsentCollection {
+      promotions: String
+    }
+
+    type SessionCustomerDetails {
+      email: String
+      phone: String
+      tax_exempt: String
+      tax_ids: [SessionCustomerDetailsTaxId]
+    }
+
+    type SessionCustomerDetailsTaxId {
+      type: String!
+      value: String
+    }
+
+    type SessionPhoneNumberCollection {
+      enabled: Boolean
+    }
+
+    type SessionShipping {
+      address: Address
+      carrier: String
+      name: String
+      phone: String
+      tracking_number: String
+    }
+
+    type SessionShippingAddressCollection {
+      allowed_countries: [String!]!
+    }
+
+    type SessionShippingOption {
+      shipping_amount: Int!
+      # shipping_rate: string | Stripe.ShippingRate;
+    }
+
+    type SessionTaxIdCollection {
+      enabled: Boolean
+    }
+
+    type SessionTotalDetails {
+      amount_discount: Int!
+      amount_shipping: Int
+      amount_tax: Int!
+      breakdown: SessionTotalDetailsBreakdown
+    }
+
+    type SessionTotalDetailsBreakdown {
+      discounts: [SessionTotalDetailsBreakdownDiscount!]!
+      taxes: [SessionTotalDetailsBreakdownTax!]!
+    }
+
+    type SessionTotalDetailsBreakdownDiscount {
+      amount: Int!
+      # discount: Stripe.Discount;
+    }
+
+    type SessionTotalDetailsBreakdownTax {
+      amount: Int!
+      # rate: Stripe.TaxRate;
     }
 
     type LineItem {
@@ -141,6 +223,10 @@ export const StripeCheckoutSession: GqlTypeDefinition<
           throw new Error(error.message);
         }
       }
+    },
+    SessionAfterExpirationRecovery: {
+      expires_at: (source: Stripe.Checkout.Session.AfterExpiration.Recovery) =>
+        timestampToDate(source.expires_at)
     },
     StripePrice: {
       id_ext: (source) => source.id,
