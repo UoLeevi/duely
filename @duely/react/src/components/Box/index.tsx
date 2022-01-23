b4dcfd9b13d0c1f9078c336fddad3861e76f7f00
Leevi Uotinen <uotinen.leevi@hotmail.com>
2022-01-23 18:12:24 +0200

    Work on Box component.

--- @duely/react/src/components/Box/index.tsx
+++ @duely/react/src/components/Box/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createClassName } from '@duely/util';
+import { createClassName, groupBy, hasProperty } from '@duely/util';
 import { Heading, HeadingProps } from '..';
 
 export type BoxProps = {
@@ -7,11 +7,34 @@ export type BoxProps = {
   className?: string;
 };
 
-export const Box = Object.assign(BoxRoot, { Heading: BoxHeading });
+export const Box = Object.assign(BoxRoot, { Heading: BoxHeading, Action: BoxAction });
 
 export function BoxRoot({ children, className }: BoxProps) {
-  className = createClassName('pb-8 md:pb-10', className);
-  return <div className={className}>{children}</div>;
+  className = createClassName('flex flex-col pb-8 md:pb-10', className);
+
+  const groupedChildren = groupBy(React.Children.toArray(children), (child) =>
+    hasProperty(child, 'type')
+      ? child.type === BoxAction
+        ? 'action'
+        : child.type === BoxHeading
+        ? 'heading'
+        : 'content'
+      : 'content'
+  );
+
+  const heading = groupedChildren.get('heading');
+  const actions = groupedChildren.get('action');
+  const content = groupedChildren.get('content');
+
+  return (
+    <div className={className}>
+      <div className="flex justify-between space-x-3 border-b border-black/[.075] pb-3 mb-3">
+        {heading}
+        <div className="grid grid-flow-row gap-2">{actions}</div>
+      </div>
+      {content}
+    </div>
+  );
 }
 
 export type BoxHeadingProps = {
@@ -20,7 +43,7 @@ export type BoxHeadingProps = {
 } & HeadingProps;
 
 function BoxHeading({ subheading, className, ...props }: BoxHeadingProps) {
-  className = createClassName('flex flex-col border-b border-black/[.075] pb-3 mb-3', className);
+  className = createClassName('flex flex-col', className);
   return (
     <div className={className}>
       {typeof subheading !== 'string' ? (
@@ -33,4 +56,14 @@ function BoxHeading({ subheading, className, ...props }: BoxHeadingProps) {
       <Heading {...props} />
     </div>
   );
+}
+
+export type BoxActionProps = {
+  children?: React.ReactNode;
+  className?: string;
+};
+
+function BoxAction({ children, className, ...props }: BoxActionProps) {
+  className = createClassName('flex', className);
+  return <div className={className}>{children}</div>;
 }
--- duely-ui-agency/src/pages/dashboard/orders/subscriptions/index.tsx
+++ duely-ui-agency/src/pages/dashboard/orders/subscriptions/index.tsx
@@ -1,4 +1,4 @@
-import { Card, DropMenu, icons, LinkButton, PropertyValue, useQueryState } from '@duely/react';
+import { Box, Card, DropMenu, icons, LinkButton, PropertyValue, useQueryState } from '@duely/react';
 import { DashboardSection } from '../../components';
 import { Currency, formatCurrency, formatDate } from '@duely/util';
 import { Table, SkeletonText, ColoredChip } from '@duely/react';
@@ -20,179 +20,167 @@ export function DashboardOrdersSubscriptions() {
 
   return (
     <>
-      <DashboardSection
-        title="Subscriptions"
-        actions={
-          <div className="flex flex-row justify-end">
-            <LinkButton
-              dense
-              color="indigo"
-              to="subscriptions/new-subscription"
-              icon="plus.solid"
-              className="text-sm"
-            >
-              New subscription
-            </LinkButton>
-          </div>
-        }
-      >
-        <Card className="max-w-screen-lg">
-          <Table
-            items={subscriptions}
-            dense={true}
-            wrap={{ md: 6 }}
-            loading={loading}
-            error={error}
-            keyField="id"
-            rowLink={(subscription: TSubscription) => ({ to: `subscriptions/${subscription.id}` })}
+      <Box className="max-w-screen-lg">
+        <Box.Heading>Subscriptions</Box.Heading>
+        <Box.Action>
+          <LinkButton
+            dense
+            color="indigo"
+            to="subscriptions/new-subscription"
+            icon="plus.solid"
+            className="text-sm"
           >
-            <Table.Column header="Customer" span={2}>
-              {(subscription: TSubscription | null) =>
-                !subscription ? (
-                  <div className="flex flex-col space-y-1">
-                    <SkeletonText className="text-sm" />
-                    <SkeletonText className="text-xs" />
-                  </div>
-                ) : (
-                  <div className="flex flex-col space-y-1">
-                    <span className="text-sm font-medium text-gray-800 dark:text-gray-300">
-                      {subscription.customer?.name ?? subscription.customer?.email?.split('@')[0]}
-                    </span>
-                    <span className="text-xs font-medium text-gray-800 dark:text-gray-300">
-                      {subscription.customer?.email}
-                    </span>
-                  </div>
-                )
+            New subscription
+          </LinkButton>
+        </Box.Action>
+        <Table
+          items={subscriptions}
+          dense={true}
+          wrap={{ md: 6 }}
+          loading={loading}
+          error={error}
+          keyField="id"
+          rowLink={(subscription: TSubscription) => ({ to: `subscriptions/${subscription.id}` })}
+        >
+          <Table.Column header="Customer" span={2}>
+            {(subscription: TSubscription | null) =>
+              !subscription ? (
+                <div className="flex flex-col space-y-1">
+                  <SkeletonText className="text-sm" />
+                  <SkeletonText className="text-xs" />
+                </div>
+              ) : (
+                <div className="flex flex-col space-y-1">
+                  <span className="text-sm font-medium text-gray-800 dark:text-gray-300">
+                    {subscription.customer?.name ?? subscription.customer?.email?.split('@')[0]}
+                  </span>
+                  <span className="text-xs font-medium text-gray-800 dark:text-gray-300">
+                    {subscription.customer?.email}
+                  </span>
+                </div>
+              )
+            }
+          </Table.Column>
+
+          <Table.Column header="Status">
+            {(subscription: TSubscription | null) =>
+              !subscription ? (
+                <div className="flex flex-col items-center">
+                  <ColoredChip color="gray" />
+                </div>
+              ) : (
+                <div className="flex flex-col items-center">
+                  <ColoredChip
+                    text={subscription.status}
+                    color={{
+                      incomplete: 'gray',
+                      incomplete_expired: 'gray',
+                      trialing: 'blue',
+                      active: 'green',
+                      past_due: 'orange',
+                      canceled: 'gray',
+                      unpaid: 'orange'
+                    }}
+                  />
+                </div>
+              )
+            }
+          </Table.Column>
+
+          <Table.Column header="Billing">
+            {(subscription: TSubscription | null) =>
+              !subscription ? (
+                <div className="flex flex-col items-center">
+                  <ColoredChip color="gray" />
+                </div>
+              ) : (
+                <div className="flex flex-col items-center">
+                  <ColoredChip
+                    text={
+                      subscription.collection_method === 'charge_automatically' ? 'auto' : 'manual'
+                    }
+                    color={{
+                      auto: 'gray',
+                      manual: 'blue'
+                    }}
+                  />
+                </div>
+              )
+            }
+          </Table.Column>
+
+          <Table.Column header="Product" span={2}>
+            {(subscription: TSubscription | null) =>
+              !subscription ? (
+                <div className="flex flex-col space-y-1">
+                  <SkeletonText className="text-sm" />
+                </div>
+              ) : (
+                <div className="flex flex-col space-y-1">
+                  <span className="text-sm font-medium text-gray-800 dark:text-gray-300">
+                    {subscription.items[0].price?.product?.name}
+                  </span>
+                </div>
+              )
+            }
+          </Table.Column>
+
+          <Table.Column header="Price" span={2}>
+            {(subscription: TSubscription | null) =>
+              !subscription ? (
+                <div className="flex flex-col space-y-2">
+                  <SkeletonText className="text-sm" />
+                </div>
+              ) : (
+                <div className="flex flex-col space-y-2">
+                  <span className="text-sm font-medium text-gray-800 dark:text-gray-300">
+                    {formatCurrency(
+                      subscription.items[0].price?.unit_amount!,
+                      subscription.items[0].price?.currency as Currency
+                    )}{' '}
+                    /{' '}
+                    {subscription.items[0].price?.recurring?.interval_count === 1
+                      ? subscription.items[0].price?.recurring?.interval
+                      : `${subscription.items[0].price?.recurring?.interval_count} ${subscription.items[0].price?.recurring?.interval}s`}
+                  </span>
+                </div>
+              )
+            }
+          </Table.Column>
+
+          <Table.Column header="Created" span={2}>
+            {(subscription: TSubscription | null) =>
+              !subscription ? (
+                <div className="flex flex-col space-y-2">
+                  <SkeletonText className="text-sm" />
+                </div>
+              ) : (
+                <PropertyValue.Date>{subscription.created}</PropertyValue.Date>
+              )
+            }
+          </Table.Column>
+
+          <Table.Column shrink>
+            {(subscription: TSubscription | null) => {
+              if (!subscription) {
+                return <div className="text-gray-300 animate-pulse">{icons['dots-vertical']}</div>;
               }
-            </Table.Column>
 
-            <Table.Column header="Status">
-              {(subscription: TSubscription | null) =>
-                !subscription ? (
-                  <div className="flex flex-col items-center">
-                    <ColoredChip color="gray" />
-                  </div>
-                ) : (
-                  <div className="flex flex-col items-center">
-                    <ColoredChip
-                      text={subscription.status}
-                      color={{
-                        incomplete: 'gray',
-                        incomplete_expired: 'gray',
-                        trialing: 'blue',
-                        active: 'green',
-                        past_due: 'orange',
-                        canceled: 'gray',
-                        unpaid: 'orange'
-                      }}
-                    />
-                  </div>
-                )
-              }
-            </Table.Column>
+              return (
+                <DropMenu>
+                  <DropMenu.Item icon={icons.pencil} to={`subscriptions/${subscription.id}/edit`}>
+                    Edit
+                  </DropMenu.Item>
 
-            <Table.Column header="Billing">
-              {(subscription: TSubscription | null) =>
-                !subscription ? (
-                  <div className="flex flex-col items-center">
-                    <ColoredChip color="gray" />
-                  </div>
-                ) : (
-                  <div className="flex flex-col items-center">
-                    <ColoredChip
-                      text={
-                        subscription.collection_method === 'charge_automatically'
-                          ? 'auto'
-                          : 'manual'
-                      }
-                      color={{
-                        auto: 'gray',
-                        manual: 'blue'
-                      }}
-                    />
-                  </div>
-                )
-              }
-            </Table.Column>
-
-            <Table.Column header="Product" span={2}>
-              {(subscription: TSubscription | null) =>
-                !subscription ? (
-                  <div className="flex flex-col space-y-1">
-                    <SkeletonText className="text-sm" />
-                  </div>
-                ) : (
-                  <div className="flex flex-col space-y-1">
-                    <span className="text-sm font-medium text-gray-800 dark:text-gray-300">
-                      {subscription.items[0].price?.product?.name}
-                    </span>
-                  </div>
-                )
-              }
-            </Table.Column>
-
-            <Table.Column header="Price" span={2}>
-              {(subscription: TSubscription | null) =>
-                !subscription ? (
-                  <div className="flex flex-col space-y-2">
-                    <SkeletonText className="text-sm" />
-                  </div>
-                ) : (
-                  <div className="flex flex-col space-y-2">
-                    <span className="text-sm font-medium text-gray-800 dark:text-gray-300">
-                      {formatCurrency(
-                        subscription.items[0].price?.unit_amount!,
-                        subscription.items[0].price?.currency as Currency
-                      )}{' '}
-                      /{' '}
-                      {subscription.items[0].price?.recurring?.interval_count === 1
-                        ? subscription.items[0].price?.recurring?.interval
-                        : `${subscription.items[0].price?.recurring?.interval_count} ${subscription.items[0].price?.recurring?.interval}s`}
-                    </span>
-                  </div>
-                )
-              }
-            </Table.Column>
-
-            <Table.Column header="Created" span={2}>
-              {(subscription: TSubscription | null) =>
-                !subscription ? (
-                  <div className="flex flex-col space-y-2">
-                    <SkeletonText className="text-sm" />
-                  </div>
-                ) : (
-                  <PropertyValue.Date>{subscription.created}</PropertyValue.Date>
-                )
-              }
-            </Table.Column>
-
-            <Table.Column shrink>
-              {(subscription: TSubscription | null) => {
-                if (!subscription) {
-                  return (
-                    <div className="text-gray-300 animate-pulse">{icons['dots-vertical']}</div>
-                  );
-                }
-
-                return (
-                  <DropMenu>
-                    <DropMenu.Item icon={icons.pencil} to={`subscriptions/${subscription.id}/edit`}>
-                      Edit
-                    </DropMenu.Item>
-
-                    <DropMenu.Item
-                      icon={icons.trash}
-                      to={'?delete_subscription=' + subscription.id}
-                    >
-                      Delete
-                    </DropMenu.Item>
-                  </DropMenu>
-                );
-              }}
-            </Table.Column>
-          </Table>
-        </Card>
-      </DashboardSection>
+                  <DropMenu.Item icon={icons.trash} to={'?delete_subscription=' + subscription.id}>
+                    Delete
+                  </DropMenu.Item>
+                </DropMenu>
+              );
+            }}
+          </Table.Column>
+        </Table>
+      </Box>
     </>
   );
 }
